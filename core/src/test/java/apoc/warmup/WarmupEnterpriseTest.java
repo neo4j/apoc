eb13787dc54982b8af44bea58b7047e2b350a7a5
Gemma Lamont <gemma.lamont@neotechnology.com>
2022-09-07 13:15:12 +0200

    Add positive tests for warmup

--- core/src/test/java/apoc/warmup/WarmupEnterpriseTest.java
+++ core/src/test/java/apoc/warmup/WarmupEnterpriseTest.java
@@ -2,8 +2,6 @@ package apoc.warmup;
 
 import apoc.util.Neo4jContainerExtension;
 import apoc.util.TestContainerUtil;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.neo4j.configuration.GraphDatabaseInternalSettings;
 import org.neo4j.configuration.GraphDatabaseSettings;
@@ -13,29 +11,39 @@ import java.util.List;
 
 import static apoc.util.TestContainerUtil.createEnterpriseDB;
 import static apoc.util.TestContainerUtil.testCall;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class WarmupEnterpriseTest {
 
-    private static Neo4jContainerExtension neo4jContainer;
-    private static Session session;
-
-    @BeforeClass
-    public static void beforeAll() {
-        neo4jContainer = createEnterpriseDB(List.of(TestContainerUtil.ApocPackage.CORE), true)
+    @Test(expected = RuntimeException.class)
+    public void testWarmupIsntAllowedWithOtherStorageEngines() {
+        Neo4jContainerExtension neo4jContainer = createEnterpriseDB(List.of(TestContainerUtil.ApocPackage.CORE), true)
                 .withNeo4jConfig(GraphDatabaseInternalSettings.include_versions_under_development.name(), "true")
                 .withNeo4jConfig(GraphDatabaseSettings.db_format.name(), "freki");
         neo4jContainer.start();
-        session = neo4jContainer.getSession();
-    }
+        Session session = neo4jContainer.getSession();
 
-    @AfterClass
-    public static void afterAll() {
+        testCall(session, "CALL apoc.warmup.run()", (r) -> {});
+
         session.close();
         neo4jContainer.close();
     }
 
-    @Test(expected = RuntimeException.class)
-    public void testWarmupIsntAllowedWithOtherStorageEngines() {
-        testCall(session, "CALL apoc.warmup.run()", (r) -> {});
+    @Test
+    public void testWarmupOnEnterpriseStorageEngine() {
+        Neo4jContainerExtension neo4jContainer = createEnterpriseDB(List.of(TestContainerUtil.ApocPackage.CORE), true)
+                .withNeo4jConfig(GraphDatabaseInternalSettings.include_versions_under_development.name(), "true")
+                .withNeo4jConfig(GraphDatabaseSettings.db_format.name(), "high_limit");
+        neo4jContainer.start();
+        Session session = neo4jContainer.getSession();
+
+        testCall(session, "CALL apoc.warmup.run(true,true,true)", r -> {
+            assertEquals(true, r.get("indexesLoaded"));
+            assertNotEquals( 0L, r.get("indexPages") );
+        });
+
+        session.close();
+        neo4jContainer.close();
     }
 }
