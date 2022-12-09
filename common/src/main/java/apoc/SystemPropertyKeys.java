60731373db3f5a0f2f6b2dca30f89628fa747a9c
Louise Söderström <louise.matematik@gmail.com>
2022-12-09 11:06:14 +0100

    [wokdlhX3] Break out parts of SystemLabels and SystemPropertyKeys

--- common/src/main/java/apoc/SystemPropertyKeys.java
+++ common/src/main/java/apoc/SystemPropertyKeys.java
@@ -7,24 +7,9 @@ public enum SystemPropertyKeys  {
     // cypher stored procedures/functions
     lastUpdated,
     statement,
-    inputs,
-    description,
-    mode,
-    outputs,
-    output,
-    forceSingle,
-    prefix,
 
     // triggers
     selector,
     params,
     paused,
-
-    // dv
-    data,
-    
-    // uuid handler
-    label,
-    addToSetLabel,
-    propertyName
 }
