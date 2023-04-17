ecec458e39e51b8f2e2dca7218b19afdf01c94ac
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-04-17 09:20:57 +0200

    [s7Ob7Zm7] Test that all load and import procedures respect security (#360)
    * [s7Ob7Zm7] Test that all load and import procedures respect security
    
    * [3QAbWQds] changed ExportCoreSecurityTest consistent with LoadImport one

--- test-utils/src/main/java/apoc/util/SensitivePathGenerator.java
+++ test-utils/src/main/java/apoc/util/SensitivePathGenerator.java
@@ -1,9 +1,7 @@
 package apoc.util;
 
 import org.apache.commons.lang3.tuple.Pair;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,16 +13,14 @@ public class SensitivePathGenerator {
     /**
      * It will return an instance of Pair<String, String> where first is the relative path
      * and other the absolute path of "etc/passwd"
-     * @param db
      * @return
      */
-    public static Pair<String, String> etcPasswd(GraphDatabaseAPI db) {
-        return base(db, "/etc/passwd");
+    public static Pair<String, String> etcPasswd() {
+        return base("/etc/passwd");
     }
 
-    private static Pair<String, String> base(GraphDatabaseAPI db, String path) {
-        final Path dbPath = db.databaseLayout().databaseDirectory();
-        final String relativeFileName = IntStream.range(0, dbPath.getNameCount())
+    private static Pair<String, String> base(String path) {
+        final String relativeFileName = IntStream.range(0, Paths.get("").toAbsolutePath().getNameCount())
                 .mapToObj(i -> "..")
                 .collect(Collectors.joining("/")) + path;
         final String absoluteFileName = Paths.get(relativeFileName)
