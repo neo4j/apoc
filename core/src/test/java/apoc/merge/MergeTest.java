8b470ff399050f20ef8ff4880a2e2cb3ba9a61a6
Nadja Müller <nadja.muller@neotechnology.com>
2023-10-03 17:38:03 +0200

    [pLz5YbCW] order tests

--- core/src/test/java/apoc/merge/MergeTest.java
+++ core/src/test/java/apoc/merge/MergeTest.java
@@ -61,6 +61,7 @@ public class MergeTest {
         testMergeNodeCommon(true);
     }
 
+    // MERGE NODES
     private void testMergeNodeCommon(boolean isWithStats) {
         String procName = isWithStats ? "nodeWithStats" : "node";
 
@@ -112,33 +113,6 @@ public class MergeTest {
     }
 
     @Test
-    public void testMergeRelationships() {
-        db.executeTransactionally("create (:Person{name:'Foo'}), (:Person{name:'Bar'})");
-
-        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'KNOWS', {rid:123}, {since:'Thu'}, e) YIELD rel RETURN rel",
-                (row) -> {
-                    Relationship rel = (Relationship) row.get("rel");
-                    assertEquals("KNOWS", rel.getType().name());
-                    assertEquals(123L, rel.getProperty("rid"));
-                    assertEquals("Thu", rel.getProperty("since"));
-                });
-
-        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'KNOWS', {rid:123}, {since:'Fri'}, e) YIELD rel RETURN rel",
-                (row) -> {
-                    Relationship rel = (Relationship) row.get("rel");
-                    assertEquals("KNOWS", rel.getType().name());
-                    assertEquals(123L, rel.getProperty("rid"));
-                    assertEquals("Thu", rel.getProperty("since"));
-                });
-        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'OTHER', null, null, e) YIELD rel RETURN rel",
-                (row) -> {
-                    Relationship rel = (Relationship) row.get("rel");
-                    assertEquals("OTHER", rel.getType().name());
-                    assertTrue(rel.getAllProperties().isEmpty());
-                });
-    }
-
-    @Test
     public void testMergeWithEmptyIdentityPropertiesShouldFail() {
         for (String idProps: new String[]{"null", "{}"}) {
             try {
@@ -174,6 +148,68 @@ public class MergeTest {
     }
 
     @Test
+    public void testEscapeIdentityPropertiesWithSpecialCharactersShouldWork() {
+        for (String key: new String[]{"normal", "i:d", "i-d", "i d"}) {
+            Map<String, Object> identProps = MapUtil.map(key, "value");
+            Map<String, Object> params = MapUtil.map("identProps", identProps);
+
+            testCall(db, "CALL apoc.merge.node(['Person'], $identProps) YIELD node RETURN node", params,
+                    (row) -> {
+                        Node node = (Node) row.get("node");
+                        assertNotNull(node);
+                        assertTrue(node.hasProperty(key));
+                        assertEquals("value", node.getProperty(key));
+                    });
+        }
+    }
+
+    @Test
+    public void testLabelsWithSpecialCharactersShouldWork() {
+        for (String label: new String[]{"Label with spaces", ":LabelWithColon", "label-with-dash", "LabelWithUmlautsÄÖÜ"}) {
+            Map<String, Object> params = MapUtil.map("label", label);
+            testCall(db, "CALL apoc.merge.node([$label],{id:1}, {name:'John'}) YIELD node RETURN node", params,
+                    row -> assertTrue(row.get("node") instanceof Node));
+        }
+    }
+
+    // MERGE RELATIONSHIPS
+    @Test
+    public void testMergeRelationships() {
+        db.executeTransactionally("create (:Person{name:'Foo'}), (:Person{name:'Bar'})");
+
+        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'KNOWS', {rid:123}, {since:'Thu'}, e) YIELD rel RETURN rel",
+                (row) -> {
+                    Relationship rel = (Relationship) row.get("rel");
+                    assertEquals("KNOWS", rel.getType().name());
+                    assertEquals(123L, rel.getProperty("rid"));
+                    assertEquals("Thu", rel.getProperty("since"));
+                });
+
+        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'KNOWS', {rid:123}, {since:'Fri'}, e) YIELD rel RETURN rel",
+                (row) -> {
+                    Relationship rel = (Relationship) row.get("rel");
+                    assertEquals("KNOWS", rel.getType().name());
+                    assertEquals(123L, rel.getProperty("rid"));
+                    assertEquals("Thu", rel.getProperty("since"));
+                });
+        testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e CALL apoc.merge.relationship(s, 'OTHER', null, null, e) YIELD rel RETURN rel",
+                (row) -> {
+                    Relationship rel = (Relationship) row.get("rel");
+                    assertEquals("OTHER", rel.getType().name());
+                    assertTrue(rel.getAllProperties().isEmpty());
+                });
+    }
+
+    @Test
+    public void testRelationshipTypesWithSpecialCharactersShouldWork() {
+        for (String relType: new String[]{"Reltype with space", ":ReltypeWithCOlon", "rel-type-with-dash"}) {
+            Map<String, Object> params = MapUtil.map("relType", relType);
+            testCall(db, "CREATE (a), (b) WITH a,b CALL apoc.merge.relationship(a, $relType, null, null, b) YIELD rel RETURN rel", params,
+                    row -> assertTrue(row.get("rel") instanceof Relationship));
+        }
+    }
+
+    @Test
     public void testMergeRelWithNullRelTypeShouldFail() {
         try {
             testCall(db, "MERGE (s:Person{name:'Foo'}) MERGE (e:Person{name:'Bar'}) WITH s,e  CALL apoc.merge.relationship(s, null, null, null, e) YIELD rel RETURN rel",
@@ -195,44 +231,7 @@ public class MergeTest {
         }
     }
 
-    @Test
-    public void testEscapeIdentityPropertiesWithSpecialCharactersShouldWork() {
-        for (String key: new String[]{"normal", "i:d", "i-d", "i d"}) {
-            Map<String, Object> identProps = MapUtil.map(key, "value");
-            Map<String, Object> params = MapUtil.map("identProps", identProps);
-            
-            testCall(db, "CALL apoc.merge.node(['Person'], $identProps) YIELD node RETURN node", params,
-                        (row) -> {
-                            Node node = (Node) row.get("node");
-                            assertNotNull(node);
-                            assertTrue(node.hasProperty(key));
-                            assertEquals("value", node.getProperty(key));
-                        });
-        }
-    }
-    
-    @Test
-    public void testLabelsWithSpecialCharactersShouldWork() {
-        for (String label: new String[]{"Label with spaces", ":LabelWithColon", "label-with-dash", "LabelWithUmlautsÄÖÜ"}) {
-            Map<String, Object> params = MapUtil.map("label", label);
-            testCall(db, "CALL apoc.merge.node([$label],{id:1}, {name:'John'}) YIELD node RETURN node", params,
-                    row -> assertTrue(row.get("node") instanceof Node));
-        }
-    }
-
-    @Test
-    public void testRelationshipTypesWithSpecialCharactersShouldWork() {
-        for (String relType: new String[]{"Reltype with space", ":ReltypeWithCOlon", "rel-type-with-dash"}) {
-            Map<String, Object> params = MapUtil.map("relType", relType);
-            testCall(db, "CREATE (a), (b) WITH a,b CALL apoc.merge.relationship(a, $relType, null, null, b) YIELD rel RETURN rel", params,
-                    row -> assertTrue(row.get("rel") instanceof Relationship));
-        }
-    }
-
-
     // MERGE EAGER TESTS
-
-
     @Test
     public void testMergeEagerNode() {
         testMergeEagerCommon(false);
