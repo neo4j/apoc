e5f80335952229d0a2694ae4c1361c79de0adbe3
Gemma Lamont <gemma.lamont@neotechnology.com>
2026-08-18 16:17:22 +0200

    Fix class resolution failures

--- common/src/main/java/apoc/util/Util.java
+++ common/src/main/java/apoc/util/Util.java
@@ -131,6 +131,7 @@ import org.neo4j.values.storable.Values;
  */
 public class Util {
 
+    private static final boolean PRINT_CLASS_CHECK_STACK_TRACES = Boolean.getBoolean("printClassCheckStackTracers");
     public static final Label[] NO_LABELS = new Label[0];
     public static final String NODE_COUNT = "MATCH (n) RETURN count(*) as result";
     public static final String REL_COUNT = "MATCH ()-->() RETURN count(*) as result";
@@ -867,7 +868,10 @@ public class Util {
         try {
             Class.forName(className);
             return true;
-        } catch (ClassNotFoundException cnfe) {
+        } catch (Throwable e) {
+            if (PRINT_CLASS_CHECK_STACK_TRACES) {
+                e.printStackTrace();
+            }
             return false;
         }
     }
